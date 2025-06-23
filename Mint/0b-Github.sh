b79a9d6ab459b230dd46218199b9ecbc1c79d81d
victormuelacarriles <victormuelacarriles@users.noreply.github.com>
2025-06-23 15:58:10 +0200

    GIT

--- Mint/0b-Github.sh
+++ Mint/0b-Github.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 #Script que descarga desde GIT la última versión de los scripts de instalación de IESMHP
 #y los copia a /LiveCDiesmhp, y ejecuta el script de configuración del LiveCD
+set -e
 
 RAIZSCRIPTSLIVE="/LiveCDiesmhp"
 SCRIPT0="$RAIZSCRIPTSLIVE/Mint/1-SetupLiveCD.sh"
+RAIZLOG="/var/log/iesmhpLinux/"
 GITREPO="https://github.com/victormuelacarriles/IAC-IESMHP.git"
-
 echoverde() {  
     echo -e "\033[32m$1\033[0m" 
 }
-echover "$($0) - Preconfiguramos equipo para ejecutar la última versión de los scripts de instalación de IESMHP"
-set -e
-setxkbmap es
+
+echoverde "$($0) - Preconfiguramos equipo para ejecutar la última versión de los scripts de instalación de IESMHP"
+
+#En español (si se puede) y con usuario mint:mint por si hay que depurar
+setxkbmap es | true
 echo "mint:mint" | chpasswd
-rm /etc/apt/sources.list
+
+rm /etc/apt/sources.list | true
 #mv /etc/apt/sources.list /sources.list.bak
 apt-get update
 #Instalamos ssh y vmware-tools
 apt-get install -y ssh git
 
 git clone $GITREPO $RAIZSCRIPTSLIVE
-/bin/bash $SCRIPT0
+chmod +x $RAIZSCRIPTSLIVE/Mint/*.sh
+mkdir -p $RAIZLOG
+/bin/bash $SCRIPT0 | tee -a $RAIZLOG/$SCRIPT0.log
+
+#FALLA!!!! el script 1-SetupLiveCD.sh no se ejecuta correctamente y se detiene. Es necesario crear log y depurarlo
+
 
