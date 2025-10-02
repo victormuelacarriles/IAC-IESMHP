297666d0001251de5b909f99571af35149a76784
victormuelacarriles <victormuelacarriles@users.noreply.github.com>
2025-10-02 17:03:17 +0200

    Update actualizaGit.sh to clone Git repository

--- Mint/utiles/actualizaGit.sh
+++ Mint/utiles/actualizaGit.sh
@@ -1,5 +1,8 @@
 apt install git -y
 GITREPO="https://github.com/victormuelacarriles/IAC-IESMHP.git"
 OUT="/opt/iesmhpMint/"
+cd /opt
 rm -r $OUT 2>/dev/null ||true
 git clone $GITREPO $OUT
+cd $OUT
+
