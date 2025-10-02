edb932316cbda62079cbd712d193b614633d1ca3
victormuelacarriles <victormuelacarriles@users.noreply.github.com>
2025-10-02 17:18:00 +0200

    Refactor actualizaGit.sh for improved cloning and execution
    Updated the script to clone the Git repository into a subdirectory and execute Auto-Ansible.sh.

--- Mint/utiles/actualizaGit.sh
+++ Mint/utiles/actualizaGit.sh
@@ -1,8 +1,12 @@
 apt update && apt install git -y
 GITREPO="https://github.com/victormuelacarriles/IAC-IESMHP.git"
-OUT="/opt/iesmhpMint/"
+OUT="/opt/iesmhpMint"
 cd /opt
-rm -r $OUT 2>/dev/null ||true
-git clone $GITREPO $OUT
-cd $OUT
-
+rm -r "$OUT/" 2>/dev/null ||true
+git clone $GITREPO "$OUT/"
+cd "$OUT/Mint/utiles"
+sh ./Auto-Ansible.sh
+cd "/opt/iesmhpMint/Mint/ansible/ProbandoRoles"
+clear
+ls
+echo 'ansible-playbook -i localhost, --connection=local roles.yaml --ssh-extra-args="-o StrictHostKeyChecking=no"'
