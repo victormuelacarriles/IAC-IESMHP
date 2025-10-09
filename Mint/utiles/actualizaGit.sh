0f646641ac5e7284f1e5f4e6a5bf336581b2a4fa
victormuelacarriles <victormuelacarriles@users.noreply.github.com>
2025-10-09 16:51:43 +0200

    Actualizada carpeta de roles

--- Mint/utiles/actualizaGit.sh
+++ Mint/utiles/actualizaGit.sh
@@ -6,5 +6,5 @@ rm -r "$OUT/" 2>/dev/null ||true
 git clone $GITREPO "$OUT/"
 cd "$OUT/Mint/utiles"
 chmod +x *.sh
-cd "/opt/iesmhpMint/Mint/ansible/ProbandoRoles"
+cd "/opt/iesmhpMint/Mint/ansible/"
 #echo 'ansible-playbook -i localhost, --connection=local roles.yaml --ssh-extra-args="-o StrictHostKeyChecking=no"'
