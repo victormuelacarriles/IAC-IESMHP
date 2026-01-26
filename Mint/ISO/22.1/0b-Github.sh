236705ea09d3a8f4a3de42a1a1506545486a9f8b
Víctor Muela Carriles <victor.muelacarriles@educantabria.es>
2026-01-26 08:03:17 +0100

    modified:   Mint/ISO/22.1/0b-Github.sh

--- Mint/ISO/22.1/0b-Github.sh
+++ Mint/ISO/22.1/0b-Github.sh
@@ -2,7 +2,7 @@
 #Script que descarga desde GIT la última versión de los scripts de instalación de IESMHP
 #y los copia a /LiveCDiesmhp, y ejecuta el script de configuración del LiveCD
 set -e
-VERSIONSCRIPT="2.00"       #Versión del script
+VERSIONSCRIPT="3.22.1.01"       #Versión del script
 SCRIPT1NOMBRE="1-SetupLiveCD.sh"
 DISTRO="Mint"
 RAIZSCRIPTSLIVE="/LiveCDiesmhp"
