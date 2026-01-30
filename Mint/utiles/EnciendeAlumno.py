e012d6a378709d874d879ab9e07c06876c4d92e0
victormuelacarriles <victormuelacarriles@users.noreply.github.com>
2026-01-30 09:25:51 +0100

    modified:   Mint/utiles/EnciendeAlumno.py

--- Mint/utiles/EnciendeAlumno.py
+++ Mint/utiles/EnciendeAlumno.py
@@ -30,11 +30,16 @@ DATOS_USUARIOS = [
     ["SMRV09", "SMRD-14", "10.0.32.134", "bc:fc:e7:05:73:63", "Natalija","A"],
     ["SMRV29", "SMRD-15", "10.0.32.135", "bc:fc:e7:05:73:c8", "Aingeru","A"],
     ["SMRV13", "SMRD-18", "10.0.32.138", "bc:fc:e7:05:73:d5", "Ignacio","A"],
+    #Añadidos Belén
+    ["SMRV08", "SMRD-13", "10.0.32.133", "bc:fc:e7:05:73:d5", "Daniel Karol","A"],
+    ["SMRV30", "SMRD-16", "10.0.32.136", "bc:fc:e7:05:73:d5", "Jorge","A"],
+    ["SMRV32", "SMRD-18", "10.0.32.138", "bc:fc:e7:05:73:d5", "Flavio","A"],
+    ["SMRV21", "SMRD-07", "10.0.32.127", "bc:fc:e7:05:71:60", "AlvaroGN","A"]
     ###################################################################### Profesores Distancia
     ["rmachog", "SMRD-15", "10.0.32.135", "bc:fc:e7:05:73:c8", "Roberto","P"],
     ["mbalava", "SMRD-15", "10.0.32.135", "bc:fc:e7:05:73:c8", "Belén","P"],
-    ["luis", "SMRD-16", "10.0.32.136", "74:56:3C:95:EC:10", "Luis M.V.","P"],
-    ["afernandez", "SMRD-17", "10.0.32.137", "74:56:3C:95:EB:87", "Angélica","P"],
+    ["luis", "SMRD-16", "10.0.32.136", "bc:fc:e7:05:73:d5", "Luis M.V.","P"],
+    ["afernandez", "SMRD-17", "10.0.32.137", "bc:fc:e7:05:73:d0", "Angélica","P"],
     ["vmuela", "SMRD-18", "10.0.32.138", "bc:fc:e7:05:73:d5", "Víctor", "P"], 
     ########################################################################################## Alumnos CEIABD
     ["CEGSIABD09","IABD-01", "10.0.72.121", "d8:bb:c1:38:71:6f", "Canales De la Hera, María del Carmen","A"],
