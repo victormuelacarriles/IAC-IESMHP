ae889e1f48e09d532a36d77a6062469366cd4beb
victormuelacarriles <victormuelacarriles@users.noreply.github.com>
2026-04-29 11:31:48 +0200

    modified:   Ubuntu/ISO/26.04/0b-Github.sh modified:   Ubuntu/ISO/26.04/0c-comprobaciones.sh

--- Ubuntu/ISO/26.04/0b-Github.sh
+++ Ubuntu/ISO/26.04/0b-Github.sh
@@ -50,6 +50,26 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y \
 dpkg --triggers-only -a --force-unsafe-io 2>/dev/null &
 log "git instalado."
 
+#FAlla: podemos ver con esto 
+# echo "1. Ver qué procesos están corriendo en ese momento:"
+# ps axf | grep -E "(apt|dpkg|git|iac|bash)" --color=never
+# echo "2. Ver en qué función de kernel está bloqueado dpkg (el más útil):"
+# cat /proc/$(pgrep -f dpkg | head -1)/wchan 2>/dev/null
+
+# echo "3. Comprobar si hay locks de apt/dpkg activos:"
+# ls -la /var/lib/dpkg/lock* /var/lib/apt/lists/lock* 2>/dev/null
+# fuser /var/lib/dpkg/lock /var/lib/apt/lists/lock 2>/dev/null
+
+# echo "4. Ver si systemd está haciendo algo:"
+# systemctl status --no-pager | head -20
+# journalctl -n 30 --no-pager
+
+# echo "5. Ver los triggers dpkg pendientes:"
+# ls /var/lib/dpkg/triggers/
+
+
+
+
 # ─────────────── Clonar repo ───────────
 if [[ -d "${DESTDIR}/.git" ]]; then
     warn "El repositorio ya existe en ${DESTDIR}. Actualizando..."
