fe3421065f1ea30a42c081c54bad1afc9043adbe
victormuelacarriles <victormuelacarriles@users.noreply.github.com>
2026-06-05 17:59:06 +0200

    modified:    Ubuntu/ISO/26.04/como usarlo.txt    Ubuntu/ansible/roles.sh    Ubuntu/ansible/roles/vmware/tasks/main.yml

--- Ubuntu/ansible/roles.sh
+++ Ubuntu/ansible/roles.sh
@@ -1,3 +1,5 @@
+############################################################################################################
 #Para ejecutar:
  cd /opt/IAC-IESMHP/Ubuntu/ansible/
  ansible-playbook -i localhost, --connection=local roles.yaml --ssh-extra-args="-o StrictHostKeyChecking=no"
+############################################################################################################
